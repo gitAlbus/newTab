eb65f7ce7d982a66ff9f07ecde8dfcc918e2f227
gitAlbus <pablitobeltran81@gmail.com>
2022-01-31 23:59:47 -0500

    This is the first Version

---
+++ index.js
@@ -0,0 +1,6 @@
+const user = new Object (); 
+user.username = 'albus';
+user.eMail = 'pablitobeltran81@gmail.com';
+user.place = 'Cuenca';
+user.membership = true;
+user.function = ['program', 'desing', 'entertainment', 'study'];
